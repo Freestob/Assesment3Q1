f274bb57c2fcf37a6a1e18fdbd07982f745f68c5
Bradley Freestone <bradley.freestone@gmail.com>
2018-09-04 13:47:02 -0400

    First commit

---
+++ Assesment3Q1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assesment3Q1.MvcApplication" Language="C#" %>
---
+++ Assesment3Q1/Models/AddNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assesment3Q1.Models
+{
+    public class AddNumbers
+    {
+        [Required]
+        [Range(1, 1000)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Count must be a natural number")]
+        public int NumberOne { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Count must be a natural number")]
+        public int NumberTwo { get; set; }
+
+    }
+}
